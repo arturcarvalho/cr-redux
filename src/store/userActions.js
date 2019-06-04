1c0af19ded59ab73a66d4473b21d20a5b3aca086
Artur Carvalho <arturbelico@gmail.com>
2019-06-04 16:23:15 +0200

    add async function for login

--- src/store/userActions.js
+++ src/store/userActions.js
@@ -1,8 +1,15 @@
 import at from "./actionTypes";
 
 const fetchUser = userId => {
-  console.log("fetching user", userId);
-  return fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+  // console.log("fetching user", userId);
+  return fetch(`https://jsonplaceholder.typicode.com/users/${userId}`).then(
+    response => {
+      if (!response.ok) {
+        throw new Error("Not 200 response");
+      }
+      return response.json();
+    }
+  );
 };
 
 const fetchUserRequest = () => ({ type: at.FETCH_USER_REQUEST });
@@ -10,21 +17,27 @@ const fetchUserSuccess = user => ({ type: at.FETCH_USER_SUCCESS, user });
 const fetchUserError = () => ({ type: at.FETCH_USER_ERROR });
 
 // I need the thunk to be able to return a function
-export function login(userId) {
+// NOT BEING USED, BUT IT'S THE SAME AS THE ASYNC LOGIN FUNCTION BELOW
+export function login1(userId) {
   return dispatch => {
     dispatch(fetchUserRequest());
 
     return fetchUser(userId)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error("Not 200 response");
-        }
-        return response.json();
-      })
-
       .then(data => dispatch(fetchUserSuccess(data)))
       .catch(_err => dispatch(fetchUserError()));
   };
 }
+
+// This first line though... I always forget it's returning an async fn...
+export const login = userId => async dispatch => {
+  dispatch(fetchUserRequest());
+
+  try {
+    const data = await fetchUser(userId);
+    dispatch(fetchUserSuccess(data));
+  } catch (_err) {
+    dispatch(fetchUserError());
+  }
+};
 
 export const logout = () => ({ type: at.LOGOUT });
